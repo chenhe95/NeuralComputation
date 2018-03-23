4e440bcd0f71400e361ed00e5e9239b2b018c663
chenhe95@hotmail.com <he@DESKTOP-CPGJOFP.localdomain>
2018-03-22 22:33:24 -0400

    .

--- HW5/compute_dir_spike_rate.m
+++ HW5/compute_dir_spike_rate.m
@@ -1,4 +1,4 @@
-function [v_180, v_0, v_315, v_135, v_200, v_20] = compute_dir_spike_rate(c)
+function [output] = compute_dir_spike_rate(c)
     dataset = c.ecodes.data;
     directions = dataset(:, 3);
     total_spike_count = [];
@@ -15,11 +15,15 @@ function [v_180, v_0, v_315, v_135, v_200, v_20] = compute_dir_spike_rate(c)
         end
     end
     
-    v_180 = total_spike_count(181) / total_view_time(181);
     v_0 = total_spike_count(1) / total_view_time(1);
-    v_315 = total_spike_count(316) / total_view_time(316);
+    v_45 = total_spike_count(46) / total_view_time(46);
+    v_90 = total_spike_count(91) / total_view_time(91);
     v_135 = total_spike_count(136) / total_view_time(136);
-    v_200 = total_spike_count(201) / total_view_time(201);
-    v_20 = total_spike_count(21) / total_view_time(21);
+    v_180 = total_spike_count(181) / total_view_time(181);
+    v_225 = total_spike_count(226) / total_view_time(226);
+    v_270 = total_spike_count(271) / total_view_time(271);
+    v_315 = total_spike_count(316) / total_view_time(316);
+    
+    output = [v_0, v_45, v_90, v_135, v_180, v_225, v_270, v_315];
 end
 
--- HW5/hw5_hechen_problem3.m
+++ HW5/hw5_hechen_problem3.m
@@ -1,31 +1,28 @@
-[v_1180, v_10, v_1315, v_1135, v_1200, v_120] = compute_dir_spike_rate(cellArrayOfCells{1, 1});
-[v_2180, v_20, v_2315, v_2135, v_2200, v_220] = compute_dir_spike_rate(cellArrayOfCells{2, 1});
-[v_3180, v_30, v_3315, v_3135, v_3200, v_320] = compute_dir_spike_rate(cellArrayOfCells{3, 1});
-[v_4180, v_40, v_4315, v_4135, v_4200, v_420] = compute_dir_spike_rate(cellArrayOfCells{4, 1});
-[v_5180, v_50, v_5315, v_5135, v_5200, v_520] = compute_dir_spike_rate(cellArrayOfCells{5, 1});
-data = [180, v_1180, v_2180, v_3180, v_4180, v_5180;
-        0, v_10, v_20, v_30, v_40, v_50;
-        315, v_1315, v_2315, v_3315, v_4315, v_5315;
-        135, v_1135, v_2135, v_3135, v_4135, v_5135;
-        200, v_1200, v_2200, v_3200, v_4200, v_5200;
-        20, v_120, v_220, v_320, v_420, v_520];
-data(:, 1) = deg2rad(data(:, 1));
+output_1 = compute_dir_spike_rate(cellArrayOfCells{1, 1});
+output_2 = compute_dir_spike_rate(cellArrayOfCells{2, 1});
+output_3 = compute_dir_spike_rate(cellArrayOfCells{3, 1});
+output_4 = compute_dir_spike_rate(cellArrayOfCells{4, 1});
+output_5 = compute_dir_spike_rate(cellArrayOfCells{5, 1});
+
+outputs = [output_1; output_2; output_3; output_4; output_5];
+
+angles = deg2rad([0, 45, 90, 135, 180, 225, 270, 315]);
 figure;
-polarscatter(data(:, 1), data(:, 2))
-title("Cell 1")
+polarscatter(angles, output_1)
+title("Cell 1 (Units in Angles and 1/ms)")
 
 figure;
-polarscatter(data(:, 1), data(:, 3))
-title("Cell 2")
+polarscatter(angles, output_2)
+title("Cell 2 (Units in Angles and 1/ms)")
 
 figure;
-polarscatter(data(:, 1), data(:, 4))
-title("Cell 3")
+polarscatter(angles, output_3)
+title("Cell 3 (Units in Angles and 1/ms)")
 
 figure;
-polarscatter(data(:, 1), data(:, 5))
-title("Cell 4")
+polarscatter(angles, output_4)
+title("Cell 4 (Units in Angles and 1/ms)")
 
 figure;
-polarscatter(data(:, 1), data(:, 6))
-title("Cell 5")
+polarscatter(angles, output_5)
+title("Cell 5 (Units in Angles and 1/ms)")
